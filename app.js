3a96e730967f34a4ae00860a4c0244ec21f3a2fb
Stephanie Mitchell <stephsorandom@gmail.com>
2022-01-14 12:28:35 -0500

    visual crossing api connected and responsive with local host

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ window.addEventListener('load', () => {
     let lon;
     let lat;
 
+    //DOM elements being selected from HTML 
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             //console.log(position) shows geolocation on devtools
@@ -17,6 +23,12 @@ window.addEventListener('load', () => {
                 return response.json()}) //once api is connected, THEN grab data
             .then(data => {
                 console.log(data)
+                //now we grab the info from api we want, which is temperature and the conditions description
+                const {temp, conditions} = data.currentConditions; 
+                //Set DOM elements from the API
+                temperatureDegree.textContent = temp
+                temperatureDescription.textContent = conditions;
+                locationTimezone.textContent = data.timezone;
             })
         });
         }
