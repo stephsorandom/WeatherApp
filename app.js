60653b79619f21b5e2d39678baf1b7e211811a72
Stephanie Mitchell <stephsorandom@gmail.com>
2022-01-14 11:59:12 -0500

    switched from open weather api to visual crossing api for more pull info

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 window.addEventListener('load', () => {
     let lon;
     let lat;
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             //console.log(position) shows geolocation on devtools
             lon = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3a052b7aa0cedbb3a0bedd1890c384e`
+            // const api =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3a052b7aa0cedbb3a0bedd1890c384e`
             // const proxy = 'https://cors-anywhere.herokuapp.com/';
            
-            
+           const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?key=6MTUNN2GMYVRJE7BG2K63J5B9`
             fetch(api) //calls api
             .then(response => {
                 return response.json()}) //once api is connected, THEN grab data
@@ -22,5 +23,4 @@ window.addEventListener('load', () => {
     
 });
 
-// api key= c3a052b7aa0cedbb3a0bedd1890c384e
-
+// api key= 6MTUNN2GMYVRJE7BG2K63J5B9
