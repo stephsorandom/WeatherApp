ac35c88bd065e61535fbdae0a7974af5aa9a6437
Stephanie Mitchell <stephsorandom@gmail.com>
2022-01-13 22:45:56 -0500

    open weather app connected to javascript file

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 window.addEventListener('load', () => {
-    let long;
+    let lon;
     let lat;
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             //console.log(position) shows geolocation on devtools
-            long = position.coords.longitude;
-            lat = position.coords.latitude
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api =`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3a052b7aa0cedbb3a0bedd1890c384e`
+            // const proxy = 'https://cors-anywhere.herokuapp.com/';
+           
+            
+            fetch(api) //calls api
+            .then(response => {
+                return response.json()}) //once api is connected, THEN grab data
+            .then(data => {
+                console.log(data)
+            })
         });
-    }
+        }
+    
 });
+
+// api key= c3a052b7aa0cedbb3a0bedd1890c384e
+
